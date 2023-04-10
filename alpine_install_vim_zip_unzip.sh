5c000daf8c40c5eed2147b1c0ddb96bce03963e8
orc.li <orc_lh@163.com>
2023-04-10 18:15:56 +0800

    Update alpine_install_vim_zip_unzip.sh

--- alpine_install_vim_zip_unzip.sh
+++ alpine_install_vim_zip_unzip.sh
@@ -6,3 +6,13 @@ apk add --update zip
 
 # 安装 unzip
 apk add --update unzip
+
+# 手动安装lrzsz（配合SecureCRT上传下载文件）
+apk add wget gcc g++ make
+wget https://ohse.de/uwe/releases/lrzsz-0.12.20.tar.gz
+tar -xf lrzsz-0.12.20.tar.gz
+cd lrzsz-0.12.20/
+./configure
+make; make install
+ln -s /usr/local/bin/lrz /usr/local/bin/rz
+ln -s /usr/local/bin/lsz /usr/local/bin/sz
